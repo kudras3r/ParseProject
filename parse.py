db04209e29fb304907ca56e876585dd46fbb6d56
kudras3r <109919639+kudras3r@users.noreply.github.com>
2022-12-07 17:35:51 +0300

    parser_update
    adding study programs and comments

--- parse.py
+++ parse.py
@@ -1,5 +1,4 @@
 
-
 # Imports
 import requests
 import openpyxl
@@ -11,15 +10,40 @@ from colorama import Style, Fore, Back
 from config import *
 from openpyxl.styles import Alignment
 from openpyxl.styles import Font
-from progress.bar import IncrementalBar
 
 
-# Function for choosing user city 
-def select_city() -> str:
+# user program
+def select_program() -> str:
+    '''
+        This is a function to work in the console.
+        Accepts a city from the user.
+    '''
+    global user_program
     
+    print('Please select the study program...')
+    for program_name in PROGRAMS.keys():
+        print(Fore.CYAN + program_name + Style.RESET_ALL)
+    
+    user_program = input('>>> ')
+    user_program = user_program.lower().capitalize()
+    
+    if user_program in PROGRAMS.keys():
+        print(PROGRAMS[user_program])
+        return PROGRAMS[user_program]
+    else:
+        print(Fore.RED + 'Invalid program name!' + Style.RESET_ALL)
+        exit()
+
+
+# user city 
+def select_city() -> str:    
+    '''
+        This is a function to work in the console.
+        Accepts a city from the user.
+    '''
     global user_city
     
-    ''' First message '''
+    # First message
     print(Fore.RED + "You have launched the university parser.\nPlease select the city by which universities will be searched.\nThe result will be send you in 'json' or 'xlsx'" + Style.RESET_ALL)
     print('-'*120)
     print('Choose city. Enter the short name.')
@@ -27,25 +51,18 @@ def select_city() -> str:
     for name, short_name in CITIES.items():
         print(Fore.WHITE + f'for {name} enter: {Fore.CYAN + short_name + Style.RESET_ALL}')
     
-    ''' Take user-city '''
     user_city = input('>>> ')
         
     if user_city.upper() in CITIES.values():
-
-        user_URL = f'https://{user_city.lower()}.postupi.online/programmy-obucheniya/bakalavr/razdel-matematika-informacionnye-nauki-i-tehnologii/'
-
+        return user_city.lower()
     else:
-        print('Invalid city code! Try again.')
-        select_city()
-        
-    ''' Return user-city '''
-    return user_URL
-
+        print(Fore.RED + 'Invalid city code!' + Style.RESET_ALL)
+        exit()
 
 
 # Creating xlsx file
-def xlsx_data(data: dict, city=user_city):
-    
+def xlsx_data(data: dict, city=user_city, program=user_program) -> str:
+
     row1 = 2
     
     ''' Creating xlsx book '''
@@ -80,96 +97,120 @@ def xlsx_data(data: dict, city=user_city):
         sheet.cell(row=row1, column=2).value = univ_info.replace(',', '')
 
         row1 += 1
-    
-    
+  
             
-    ''' Saving book in ./directory '''
-    book.save(f"G:/parse_project/results/{city}_result.xlsx")
+    ''' Saving book in ./results '''
+    book.save(f"results/{city}_result_{program[1:-1]}.xlsx")
     book.close()
 
+    return f"results/{city}_result_{program[1:-1]}.xlsx"  #file name 
 
-# Data collection function(url) -> data
+
+# data collection function(url) -> data
 def get_data(URL) -> dict:
-
+    '''
+        The main function for the parser. collects all information.
+    '''
     result_dict = dict()
 
-    ''' Make request to 1st page - (get for taking cookie an posts for sending it) '''
-    s = requests.Session()
-    s.get('https://msk.postupi.online/programmy-obucheniya/bakalavr/razdel-matematika-informacionnye-nauki-i-tehnologii/', headers=headers)
+    # make request to 1st page - (get for taking cookie an posts for sending it)
+    s = requests.Session()    # Session 
+    s.get(URL, headers=headers)
     print(f'{headers} - us_agent')
     time_cookies = requests.utils.dict_from_cookiejar(s.cookies)
     
     response = s.post(URL, headers=headers, cookies=time_cookies)
     
-    ''' Check status code '''
+    # check status code
     if response.status_code == 200:
 
         print(f'status code: {response.status_code}')
         print('wait...')
         
-        ''' Create a soup for parse '''
+        # Create a soup for parse
         soup = BeautifulSoup(response.text, 'lxml')
-        navigation_count = int(soup.find('div', class_='invite fetcher').find_all('a', class_='paginator')[-1].text)
         
-        bar = IncrementalBar('processing', max = navigation_count)
-
-        ''' Iterate all pages '''
-        for page in range(1, navigation_count + 1): 
+        #    Сhecking the number of pages on the site.
+        #    If there is only one page, it goes to else
         
-            bar.next()
+        if 'invite fetcher' in response.text:
             
-            url = f'{URL}?page_num={page}'
+            navigation_count = int(soup.find('div', class_='invite fetcher').find_all('a', class_='paginator')[-1].text)
+
+            for page in range(1, navigation_count + 1): 
+        
+                print(f'{page}/{navigation_count}')
+            
+                url = f'{URL}?page_num={page}'
+                response = s.post(url, headers=headers, cookies=time_cookies)
+            
+                soup = BeautifulSoup(response.text, 'lxml')
+        
+                all_programs_on_page = soup.find('ul', class_='list-unstyled list-wrap').find_all('div', class_='list__info')
+
+                page_iterate(all_programs_on_page, result_dict, time_cookies, s)
+        
+        # If there are more than one page, iterate over each and collect information
+        else:
+            url = URL
             response = s.post(url, headers=headers, cookies=time_cookies)
-            
             soup = BeautifulSoup(response.text, 'lxml')
-        
             all_programs_on_page = soup.find('ul', class_='list-unstyled list-wrap').find_all('div', class_='list__info')
-            
-            ''' Iterate all program on pages'''
-            for program in all_programs_on_page:
+            page_iterate(all_programs_on_page, result_dict, time_cookies, s)
 
-                program_url = program.find('h2', class_='list__h').find('a')['href']
-                program_name = program.find('h2', class_='list__h').find('a').text
-                
-                if program.find('span', class_='list__score-sm') != None:
-                    
-                    min_points = program.find('span', class_='list__score-sm').find('b').text
-                    
-                else:
-                    min_points = 'No info'
-
-                if 'vuz' in program_url:
-                
-                    univ_name = program.find('p', class_='list__pre').find('a').text
-                    univ_link = program.find('h2', class_='list__h').find('a')['href']
-                
-                    result_dict[program_name] = {univ_name: univ_link}
-                    result_dict[program_name]['Баллы на бюджет: '] = min_points
-                    
-                else:
-                    univ_names = []
-                    univ_links = []
-
-                    response = s.post(program_url, headers=headers, cookies=time_cookies)
-                    soup = BeautifulSoup(response.text, 'lxml')
-
-                    univ_info = soup.find('div', class_='detail-box__item detail-box__item_upcase').find_all('a')
-
-                    for info in univ_info:
-                        univ_names.append(info.text.replace(u"\u00A0", ""))
-                        univ_links.append(info['href'])
-
-                    result_dict[program_name] = dict(zip(univ_names, univ_links))
-                    result_dict[program_name]['Баллы на бюджет: '] = min_points
         print('\n')
         print(Fore.GREEN + 'Complete!' + Style.RESET_ALL)
         print('-'*120)
 
-        return result_dict
+        return result_dict    # main data!
     
     else:
         print('Error')
         sys.exit
+
+# pages iterate
+def page_iterate(all_programs_on_page, result_dict, time_cookies, s):
+
+    ''' Iterates through the pages from the list all_programs_on_page, 
+        places the results in result_dict.
+        To work, you need to set cookies and session (s)'''
+
+    for program in all_programs_on_page:
+
+        program_url = program.find('h2', class_='list__h').find('a')['href']
+        program_name = program.find('h2', class_='list__h').find('a').text
+                
+        if program.find('span', class_='list__score-sm') != None:
+                    
+            min_points = program.find('span', class_='list__score-sm').find('b').text
+                    
+        else:
+            min_points = 'No info'
+
+        if 'vuz' in program_url:
+                
+            univ_name = program.find('p', class_='list__pre').find('a').text
+            univ_link = program.find('h2', class_='list__h').find('a')['href']
+                
+            result_dict[program_name] = {univ_name: univ_link}
+            result_dict[program_name]['Баллы на бюджет: '] = min_points
+                    
+        else:
+            univ_names = []
+            univ_links = []
+
+            response = s.post(program_url, headers=headers, cookies=time_cookies)
+            soup = BeautifulSoup(response.text, 'lxml')
+
+            univ_info = soup.find('div', class_='detail-box__item detail-box__item_upcase').find_all('a')
+
+            for info in univ_info:
+                        univ_names.append(info.text.replace(u"\u00A0", ""))
+                        univ_links.append(info['href'])
+
+            result_dict[program_name] = dict(zip(univ_names, univ_links))
+            result_dict[program_name]['Баллы на бюджет: '] = min_points
+
 
 # Function for testing (not working at main code)
 def print_result(result: dict):
@@ -184,23 +225,35 @@ def print_result(result: dict):
 
 
 # Main function
-def main():
+def json_creating():
     
-    url = select_city()
+    city = select_city()
+    prog = select_program()
+    url = 'https://' + city + '.postupi.online/programmy-obucheniya/bakalavr/razdel' + prog
 
     data = get_data(url)
     
     ''' Creating json with data'''
-    file = open(f'G:/parse_project/results/{user_city}_result.json', 'w', encoding='utf-8')
+    file = open(f'results/{city}_result.json', 'w', encoding='utf-8')
     json.dump(data, file, indent=4, ensure_ascii=False)
     file.close()    
 
-    print(Fore.GREEN + f'Succesfuly! Check {user_city}_result.json' + Style.RESET_ALL)
+    print(Fore.GREEN + f'Succesfuly! Check {city}_result.json' + Style.RESET_ALL)
 
-
+# TESTS for console
 if __name__ == '__main__':
+   
 
-    data = get_data(select_city())
-    xlsx_data(data, user_city)
-   # main()
+   #data = get_data(select_city())
+   #xlsx_data(data, user_city)
+    json_creating()
+   #prog = select_program()
+   #city = select_city()
+   #url = 'https://' + city + '.postupi.online/programmy-obucheniya/bakalavr/razdel' + prog
+   #print(url)
+   #data = get_data(url)
+   #xlsx_data(data, city, prog)
+
+    
+
     
