718714d060778814699584541e60b2cc1ae602cb
kudras3r <109919639+kudras3r@users.noreply.github.com>
2023-04-13 20:39:53 +0300

    Add files via upload

---
+++ groups.py
@@ -0,0 +1,13 @@
+
+"""
+
+University Parser - Парсер ВУЗов (Telegram Bot) - https://t.me/UnivParseBot 
+Creator - kudras3r (https://t.me/kudras3r_dev)
+Licence - COPYRIGHT ECHO'S DEVELOPMENT © 2023 
+All rigts reserved
+
+"""
+
+links = ["https://t.me/kudras3r_dev", "https://t.me/tg_main_project"]
+channels_id = ["-1001876435436", "-1001633399674"]
+
---
+++ languages_config.py
@@ -0,0 +1,42 @@
+
+"""
+
+University Parser - Парсер ВУЗов (Telegram Bot) - https://t.me/UnivParseBot 
+Creator - kudras3r (https://t.me/kudras3r_dev)
+Licence - COPYRIGHT ECHO'S DEVELOPMENT © 2023 
+All rigts reserved
+
+"""
+
+LANGUAGES = {
+    
+    'INFO': "<b>UnivParser - Парсер ВУЗов (Telegram Bot) - @UnivParseBot</b>\n\n<b>Создатель: @wa1tandble3d</b>\n\n<b>Бот относится к проекту main</b> - @tg_main_project\n\n<b>Правообладатель</b> - @wa1tandble3d\n\n<b>Лицензия</b> - ECHO'S DEVELOPMENT © 2023\n<b><i>Все права сохранены.</i></b>",
+    
+    'RUS': 
+    {
+        'sub_request_message_rus': f'\n\nДля того, чтобы продолжить, вам надо подписаться на эти каналы, а после нажать на "Я подписался ✅".</b>',
+        'first_message_from_bot_rus': f"""➖\nЭтот бот позволяет вам в кратчайшие строки получить информацию о ВУЗах России.
+                                  \nНужно лишь выбрать город и программу обучения, что вас интересуют.
+                                  \nЗатем бот все сделает за вас и предоставит JSON или XLSX файл, в котором будут находиться ссылки на ВУЗы выбранного города.""",
+        'city_choice': 'Выберите город >>>',
+        'sub_reques': 'Подписаться',
+        'sub_ok': 'Я подписался ✅',
+        'sub_no': "<b>Вы не подписались!\n\nПользоваться ботом можно только после подписки.</b>",
+        'back_btn': 'Назад ⬅',
+        'us_prog': 'Выбери программу обучения'
+
+    },
+    'ENG':
+    {
+    'sub_request_message_eng': f'\n\nTo continue, you need to subscribe to these channels, and then click on "I subscribed ✅".</b>',
+    'first_message_from_bot_eng': f"""➖\nThis bot allows you to get information about Russian universities.
+                                \nYou only need to choose the city and the study program that interests you.
+                                \nThen the bot does everything for you and includes JSON or XLSX files containing links to universities in the selected city.""",
+    'city_choice': 'Choose a city >>>',
+    'sub_reques': 'Subscribe',
+    'sub_ok': 'I subscribed ✅',
+    'sub_no': "<b>You have not subscribed!\n\nYou can use the bot only after you subscribe.</b>",
+    'back_btn': 'Back ⬅',
+    'us_prog': 'Choose study programm'    
+    }
+}
